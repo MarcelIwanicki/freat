9396ccaa292f6f1318e64752f11a465fc4f7fbf7
Marcel Iwanicki <iwanickimarcel@outlook.com>
2023-11-12 10:43:38 +0100

    changed look of IngredientsScreen item

--- shared/src/commonMain/kotlin/com/iwanickimarcel/freat/feature/add_recipe/presentation/IngredientsScreen.kt
+++ shared/src/commonMain/kotlin/com/iwanickimarcel/freat/feature/add_recipe/presentation/IngredientsScreen.kt
@@ -1,9 +1,12 @@
 package com.iwanickimarcel.freat.feature.add_recipe.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,10 +18,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import com.iwanickimarcel.freat.feature.products.domain.Product
 
 @Composable
 fun IngredientsScreen(
@@ -28,16 +31,21 @@ fun IngredientsScreen(
     LazyColumn(
         modifier = Modifier
             .padding(16.dp)
-            .fillMaxWidth(),
+            .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(addRecipeState.ingredients) {
-            Box(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .height(48.dp)
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     buildString {
@@ -47,7 +55,6 @@ fun IngredientsScreen(
                         append(" ")
                         append(it.amount.unit.abbreviation)
                     },
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
             }
         }
@@ -55,8 +62,14 @@ fun IngredientsScreen(
             IconButton(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .height(48.dp)
                     .clip(RoundedCornerShape(8.dp))
-                    .background(MaterialTheme.colorScheme.secondary),
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.onSecondaryContainer,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .background(MaterialTheme.colorScheme.secondaryContainer),
                 onClick = {
                     onAddIngredientPressed()
                 },
@@ -64,7 +77,7 @@ fun IngredientsScreen(
                     Icon(
                         imageVector = Icons.Filled.Add,
                         contentDescription = "Add ingredient",
-                        tint = MaterialTheme.colorScheme.onSecondary
+                        tint = MaterialTheme.colorScheme.onSecondaryContainer
                     )
                 }
             )
