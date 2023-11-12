117a5ae79285cd49fc15058d362eb47be160bb77
Marcel Iwanicki <iwanickimarcel@outlook.com>
2023-11-12 11:52:16 +0100

    changed look of StepsScreen item

--- shared/src/commonMain/kotlin/com/iwanickimarcel/freat/feature/add_recipe/presentation/StepsScreen.kt
+++ shared/src/commonMain/kotlin/com/iwanickimarcel/freat/feature/add_recipe/presentation/StepsScreen.kt
@@ -3,6 +3,7 @@ package com.iwanickimarcel.freat.feature.add_recipe.presentation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,10 +16,14 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun StepsScreen(
@@ -32,7 +37,25 @@ fun StepsScreen(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(addRecipeState.steps) {
-
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(8.dp),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = it.step.toString(),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 32.sp,
+                )
+                Text(it.description)
+            }
         }
         item {
             IconButton(
