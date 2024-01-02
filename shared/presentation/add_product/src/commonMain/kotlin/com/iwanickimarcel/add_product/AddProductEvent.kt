8b49e8b78867cfc45386d190cbbdc6b0c8a70522
Marcel Iwanicki <iwanickimarcel@outlook.com>
2024-01-02 15:05:17 +0100

    bugfix: fixed AddProductScreen text input

--- shared/presentation/add_product/src/commonMain/kotlin/com/iwanickimarcel/add_product/AddProductEvent.kt
+++ shared/presentation/add_product/src/commonMain/kotlin/com/iwanickimarcel/add_product/AddProductEvent.kt
@@ -1,8 +1,10 @@
 package com.iwanickimarcel.add_product
 
+import androidx.compose.ui.text.input.TextFieldValue
+
 sealed interface AddProductEvent {
     data class OnEditProductProvided(val name: String) : AddProductEvent
-    data class OnNameChanged(val name: String) : AddProductEvent
+    data class OnNameChanged(val name: TextFieldValue) : AddProductEvent
     data class OnAmountChanged(val amount: String) : AddProductEvent
     data class OnAmountUnitChanged(val unit: String) : AddProductEvent
     data class OnPhotoSelected(val photoBytes: ByteArray) : AddProductEvent
