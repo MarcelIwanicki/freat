65c08023b06128d6bafe3912be20bd4c90ec1594
Marcel Iwanicki <iwanickimarcel@outlook.com>
2024-01-14 10:47:54 +0100

    test: added HomeViewModelTest

---
+++ shared/presentation/home/src/commonTest/kotlin/com/iwanickimarcel/home/FakeRecipeDataSource.kt
@@ -0,0 +1,47 @@
+package com.iwanickimarcel.home
+
+import com.iwanickimarcel.recipes.Recipe
+import com.iwanickimarcel.recipes.RecipeDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class FakeRecipeDataSource : RecipeDataSource {
+
+    private val fakeRecipes = mutableListOf<Recipe>()
+
+    override fun getRecipes(): Flow<List<Recipe>> {
+        return flowOf(fakeRecipes)
+    }
+
+    override suspend fun getRecipeById(id: Long): Recipe {
+        return fakeRecipes.find {
+            it.id == id
+        } ?: Recipe(
+            id = id,
+            name = "",
+            photoBytes = null,
+            products = emptyList(),
+            tags = emptyList(),
+            steps = emptyList(),
+            ownedProductsPercent = 0,
+            isFavorite = false
+        )
+    }
+
+    override suspend fun insertRecipe(recipe: Recipe) {
+        fakeRecipes.removeIf {
+            it.id == recipe.id
+        }
+        fakeRecipes.add(recipe)
+    }
+
+    override suspend fun deleteRecipe(id: Long) {
+        fakeRecipes.remove(
+            fakeRecipes.find {
+                it.id == id
+            }
+        )
+    }
+
+
+}
